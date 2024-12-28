96789c4a22c9601509ff00d9d6a26fc6de8138c6
sallen <chouallenno@gmail.com>
2024-12-28 09:26:54 +0800

    adc_oneshot complete

--- adc/main/main.c
+++ adc/main/main.c
@@ -1,4 +1,34 @@
+#include "hal/adc_types.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
+#define TAG "ADC"
+
 void app_main(void)
 {
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = 
+    {
+        .unit_id = ADC_UNIT_1,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    adc_oneshot_chan_cfg_t config = 
+    {
+        .bitwidth = ADC_BITWIDTH_9,
+        .atten = ADC_ATTEN_DB_12,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &config));
+
+    int adc_read;
+    while(1)
+    {
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_read));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL_0, adc_read);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
 
 }
